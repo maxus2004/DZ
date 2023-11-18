521a4841a04e94eec77e4b8e1c4854c738b15754
Max <maxus2004123@gmail.com>
2023-11-19 00:22:25 +0300

    add all pages from old project

---
+++ app/assets/javascripts/zoom.js
@@ -0,0 +1,15 @@
+let originalFramesize = 6
+
+window.onload = async function () {
+    document.getElementById('video').onclick = goBack
+    document.getElementById('video').src = getStreamURL()
+    let response = await api('camera_config')
+    originalFramesize = response.get('framesize')
+    api('camera_config', { framesize: 13 })
+    
+}
+
+async function goBack() {
+    await api('camera_config', { framesize: originalFramesize })
+    document.location = '/'
+}
